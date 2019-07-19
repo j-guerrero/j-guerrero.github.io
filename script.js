6816078ef53b1e4b29d53923ddfb78ed88dc7b38
Josh Guerrero <jrg99654@gmail.com>
2019-07-18 21:04:38 -0800

    many changes to formatting for sm,md,lg views

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-$(document).ready(function(){
-			function isScrolledIntoView(elem){
-				var docViewTop = $(window).scrollTop();
-				var docViewBottom = docViewTop + $(window).height();
-				var elemTop = $(elem).offset().top;
-				var elemBottom = elemTop + $(elem).height();
-
-				return ((elemBottom <= docViewBottom) && (elemTop>=docViewTop));
-
-			}
-		});
-
-$(window).scroll(function(){
-	$('.scroll-animations .animated').each(function(){
-		if(isScrolledIntoView(this) === true){
-			$(this).addClass('fadeInUp');
-		}
-	})
+$(document).ready(function () {
+	window.scrollTo(0,0);
 });
